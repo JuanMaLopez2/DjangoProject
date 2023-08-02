a67565914634376bfc431cc297625c291996fc41
jmlopezs <jmlopezs@eafit.edu.co>
2023-08-02 11:52:25 -0500

    se añade el path about y se modifica el home

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,5 @@ def home(request):
     #return HttpResponse('<h1>Welcome to home Page<h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Juan Manuel López'})
+def about(request):
+    return render(request, 'about.html')
