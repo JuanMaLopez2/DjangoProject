fd8fd254fc9c4d367877330a5adc5fddd19db770
jmlopezs <jmlopezs@eafit.edu.co>
2023-08-10 00:21:28 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home Page<h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Juan Manuel López'})
+    #return render(request, 'home.html', {'name':'Juan Manuel López'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+
 def about(request):
-    return render(request, 'about.html')
+    return HttpResponse('<h1>Welcome to About Page </h1>')
